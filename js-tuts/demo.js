ffa27ebaa3059cd23471abe41fac9e531cd80585
Gaurav <gauravrawat8433022@gmail.com>
2024-04-04 15:32:09 +0530

    added css and js tutorial

---
+++ js-tuts/demo.js
@@ -0,0 +1,61 @@
+//variables
+
+// console.log(a)
+// var a=12;
+
+//conditionals
+
+// if("0")
+// {
+//     console.log("running")
+// }
+// else{
+//     console.log("not running")
+// }
+
+
+//Loops
+
+// for(var i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+
+//function
+
+// function hello()
+// {
+//     console.log("hello");
+// }
+// hello();
+
+//Arrays
+
+// var arr=[1,2,3,4,66,5]
+// for(var i=0;i<arr.length;i++) console.log(arr[i]);
+// console.log(arr);
+// arr.push(100);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.unshift(299);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.splice(2,2);
+// console.log(arr);
+
+
+//Objects
+
+//Blank Object
+
+// var obj={};
+
+//Filled object
+// var student={
+//     age:21,name:"shankar",linkedin:"jdkfsjk@gmail.com",rollno:10,  //proprtyyyyy 
+//     greet:function greet(){ console.log("good morning");}   //method
+// }
+// console.log(student.name);
+// student.greet();
