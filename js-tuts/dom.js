b0fe3a87ff8b7116a3350cab8108076a6715619c
Gaurav <gauravrawat0259@gmail.com>
2024-04-19 16:39:02 +0530

    Added js-dom tutorial

---
+++ js-tuts/dom.js
@@ -0,0 +1,52 @@
+// let head=document.querySelector("h2");
+// head.innerText+=" javascipt";
+
+
+
+// let boxes= document.querySelectorAll(".box");
+
+// let i=1;
+// boxes.forEach(box => {
+//     box.innerText=`${i++} Box`;
+//     box.style.display="flex";
+//     box.style.justifyContent="center";
+//     box.style.alignItems="center";
+//     box.style.backgroundColor=`#`+`${i+926}`;    
+// });
+
+
+// let adds=()=>
+// {
+//     let bodys=document.querySelector("body");
+//     for(let i=0;i<10;i++)
+//     {
+//         bodys.innerHTML+=`
+//         <div class="logo">
+//             <h1>
+//                 hahaha ${i}
+//             </h1>
+//         </div>
+//         `
+//     }
+// }
+// adds();
+
+
+// let hello=()=>
+// {
+// alert("heyyyyy");
+// }
+
+// let bodys=document.querySelector("body");
+// let button=document.createElement("button");
+// button.innerText="click me";
+// button.style.backgroundColor="RED";
+// button.style.color="white";
+// bodys.append(button);
+// button.addEventListener("click",hello);
+
+
+
+
+
+
---
+++ js-tuts/practice1.js
@@ -0,0 +1,13 @@
+//return no of vowels in a string
+
+// const countvowel=(s)=>{
+//    for(let i=0;i<s.length;i++)
+//    {
+//     if(s[i]==='a' || s[i]==='e'||s[i]==='i'||s[i]==='o'||s[i]==='u') count++;
+//    }
+
+// }
+// let count=0;
+// countvowel("aeiou");
+// console.log(count);
+
