2cccd4400906f9af7b564232aee33b6d063f363e
Gaurav <gauravrawat0259@gmail.com>
2024-04-19 17:17:05 +0530

    Added js-events tutorial

---
+++ js-tuts/events.js
@@ -0,0 +1,26 @@
+// let hello=()=>{
+//     let head=document.createElement("h1");
+//     head.append(document.createTextNode(`${currmode}`));
+//     let bodys=document.querySelector("body");
+//     bodys.append(head);
+// }
+
+
+// let mode=()=>{
+//     if(currmode==="light")
+//     {
+//         currmode="dark";
+//         document.querySelector("body").style.backgroundColor="black";
+//         document.querySelector("body").style.color="white";
+//     }
+//     else 
+//     {
+//         currmode="light";
+//         document.querySelector("body").style.backgroundColor="white";
+//         document.querySelector("body").style.color="black";
+//     }
+// }
+// let button=document.querySelector("#btn1");
+// let currmode="light";
+// button.addEventListener("click",mode);
+// button.addEventListener("click",hello);
