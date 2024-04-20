f77991b6b564eaab3a1b045e85393ec6b399fee6
Gaurav <gauravrawat0259@gmail.com>
2024-04-20 18:22:07 +0530

    Added js-Classes tutorial

---
+++ js-tuts/classandobjects.js
@@ -0,0 +1,49 @@
+
+//classes
+
+// class cars{
+//     makecar()
+//     {
+//         console.log("made a new car");
+//     }
+// }
+// let honda=new cars();
+// honda.makecar();
+
+
+//objects and prototype
+
+// const employee={
+//     calctax()
+//     {
+//         console.log("100 rs tax");
+//     }
+// };
+
+// const nextemployee={
+// salary:500
+// };
+
+// nextemployee.__proto__=employee;  //adding prototype
+// console.log(nextemployee);
+
+
+//constructor
+
+
+// class cars{
+//     brand="car"
+//     makecar()
+//     {
+//         console.log("made a new car");
+//     }
+//     constructor(brand)
+//     {
+//         this.brand=brand;
+//     }
+// }
+// let honda=new cars("honda");
+// console.log(honda.brand);
+
+
+
